bfcf67bc86ffcb6def413ea73b0ffdc6b8482d49
Marek Meissner <marek.meissner99@gmail.com>
2026-03-17 23:28:09 +0100

    chore: remove boilerplate comments

--- ios/VoiceStudioModule.mm
+++ ios/VoiceStudioModule.mm
@@ -1,32 +1,16 @@
 #import "VoiceStudioModule.h"
-
-/**
- * When using Swift classes in ObjC implementation, the classes must be imported
- * from generated Objective-C Interface Header
- *
- * @see https://developer.apple.com/documentation/swift/importing-swift-into-objective-c#Import-Code-Within-an-App-Target
- */
 #import "VoiceStudio-Swift.h"
-
-/**
- * Import header file with codegenerated protocols based on the JS specification
- *
- * The name of the header matches the name provided in codegenConfig's `name` field in package.json
- */
 #import "VoiceStudio.h"
 
-// Each turbo module extends codegenerated spec class
 @interface VoiceStudioModule () <VoiceStudioModuleDelegate>
 @end
 
-// Declare the ObjC implementation for that native module class
 @implementation VoiceStudioModule {
   VoiceStudioModuleImpl *moduleImpl;
   RCTPromiseResolveBlock resolveBlock;
   RCTPromiseRejectBlock rejectBlock;
 }
 
-// Return the name of the module - it should match the name provided in JS specification
 RCT_EXPORT_MODULE(VoiceStudio)
 
 - (instancetype)init {
@@ -38,7 +22,6 @@ RCT_EXPORT_MODULE(VoiceStudio)
     return self;
 }
 
-// Declare if module should be initialized on the main queue
 + (BOOL)requiresMainQueueSetup
 {
     return YES;
@@ -62,10 +45,6 @@ RCT_EXPORT_MODULE(VoiceStudio)
     rejectBlock = nil;
 }
 
-/**
- * If the module interacts with UIKit,
- * it can declare that its methods should be run on main queue
- */
 - (dispatch_queue_t)methodQueue {
     return dispatch_get_main_queue();
 }
@@ -81,7 +60,6 @@ RCT_EXPORT_METHOD(stopRecording) {
   [moduleImpl stopRecordingSession];
 }
     
-// Implement RCTTurboModule protocol
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
 {
